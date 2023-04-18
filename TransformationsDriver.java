0314785e70b668882a26d750bd13ad9f8fe4f70f
Malinda Uboh <malindauboh02@gmail.com>
2023-04-18 18:36:18 -0400

    finished setting up frame

--- TransformationsDriver.java
+++ TransformationsDriver.java
@@ -14,7 +14,16 @@ public class TransformationsDriver {
     public static Transformations passOver = new Transformations(); //Object of package class
 
     public static void main(String[] args) {
-
+        JFrame frame = new JFrame();
+        JPanel base = new JPanel();
+            base.setLayout(new FlowLayout());
+            JLabel picLabel = new JLabel(new ImageIcon(buffered)); 
+            base.add(picLabel); //adds the buffered image inside of the JPanel 
+            frame.add(base); //Adds Jpanel to frame 
+            frame.setSize(1000, 1000);
+            frame.setTitle("Generated Lines");
+            frame.pack();
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
 
 
     }
