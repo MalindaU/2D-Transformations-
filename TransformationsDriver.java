f274b330113cf17fc7bb083ee709512ce438c0d8
Malinda Uboh <malindauboh02@gmail.com>
2023-04-18 18:39:36 -0400

    Fixed visability error with frame

--- TransformationsDriver.java
+++ TransformationsDriver.java
@@ -24,7 +24,7 @@ public class TransformationsDriver {
             frame.setTitle("Generated Lines");
             frame.pack();
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-
+            frame.setVisible(true);
 
     }
 
